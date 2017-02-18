352b922e63e31afcef3163a2eb9352e5a483fe4c
Tyler Gamvrelis <ty.gamvrelis@gmail.com>
2017-02-18 04:02:40 -0500

    Fixed p wrapping. Trying to fix nav menu resize

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 /* Toggle between adding and removing the "responsive" class to topnav when the
    user clicks on the icon */
-function myFunction() {
+function resize() {
     var x = document.getElementById("myTopnav");
     if (x.className === "topnav") {
         x.className += " responsive";
